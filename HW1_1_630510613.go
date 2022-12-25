8ce7eb6d9f42e4f1542787356718114c859b411b
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2022-12-25 20:45:16 +0700

    Update HW1_1_630510613.go

--- HW1_1_630510613.go
+++ HW1_1_630510613.go
@@ -5,18 +5,30 @@ import (
 	"sync"
 	"time"
 )
+
+// Declare a WaitGroup variable
 var wg sync.WaitGroup
 
+// Declare a function that takes a string argument
 func say(s string) {
+	// Print the string
 	fmt.Println(s)
+	// Sleep for 100 milliseconds
 	time.Sleep(time.Millisecond * 100)
+	// Decrement the WaitGroup counter by 1
 	wg.Done()
 }
+
 func main() {
+	// Run a loop 5 times
 	for i := 0; i < 5; i++ {
+		// Print a message
 		fmt.Println("main : hello")
+		// Increment the WaitGroup counter by 1
 		wg.Add(1)
+		// Start a new goroutine that calls the say function
 		go say("goroutine : world!")
+		// Block the main function until the WaitGroup counter is zero
 		wg.Wait()
 	}
 }
