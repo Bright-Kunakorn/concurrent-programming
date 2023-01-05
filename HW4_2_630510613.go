39ce8636ee660f192483eae3ec648319b3bbb9e0
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-01-05 14:15:48 +0700

    Update HW4_2_630510613.go

--- HW4_2_630510613.go
+++ HW4_2_630510613.go
@@ -1,65 +1,86 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 	"sync"
+	"time"
 )
 
-const maxCustomers = 3
+var wakeBell chan bool
+var sleepBell chan bool
 
-var (
-	customers     int
-	customerMutex sync.Mutex
-	customer      = make(chan struct{})
-	barber        = make(chan struct{})
-	customerDone  = make(chan struct{})
-	barberDone    = make(chan struct{})
-)
+func barberShop(chairs chan string, ctx context.Context, wg *sync.WaitGroup) {
+	wg.Add(1)
+	sleepBell <- true
+	defer wg.Done()
+	fmt.Println("BarberShop opened")
+	defer fmt.Println("BarberShop closed")
+	closeShop := false
+	for {
+		select {
+		case id := <-chairs:
+			getHairCut(id)
+			if len(chairs) == 0 {
+				if !closeShop {
+					sleepBarber(id)
+				} else {
+					return
+				}
+			}
+		case <-wakeBell:
+			wakeBarber()
+		case <-ctx.Done():
+			if len(chairs) == 0 {
+				return
+			}
+			closeShop = true
+		}
+	}
+}
+
+func customerEnter(id int, chairs chan string) {
+	fmt.Printf("Customer %d enter BarberShop\n", id)
+	select {
+	case <-sleepBell:
+		wakeBell <- true
+		chairs <- strconv.Itoa(id)
+		fmt.Printf("customer %d sitting\n", id)
+	case chairs <- strconv.Itoa(id):
+		fmt.Printf("customer %d sitting\n", id)
+	default:
+		fmt.Printf("No chair available, customer %d leaving BarberShop\n", id)
+	}
+}
+
+func sleepBarber(id string) {
+	sleepBell <- true
+	fmt.Println("Barber sleeping")
+}
+func wakeBarber() {
+	fmt.Println("Wakeup barber")
+}
+func getHairCut(id string) {
+	fmt.Println("Cutting for customer ", id)
+	time.Sleep(1 * time.Second)
+	fmt.Println("Cutting done, customer", id)
+}
 
 func main() {
-	fmt.Print("")
-	go barberThread()
-	for i := 1; i <= 5; i++ {
-		go customerThread(i)
+	wakeBell = make(chan bool)
+	sleepBell = make(chan bool, 1)
+	chairs := make(chan string, 5)
+	wg := new(sync.WaitGroup)
+	ctx, cancel := context.WithCancel(context.Background())
+	go barberShop(chairs, ctx, wg)
+	<-sleepBell
+	for i := 1; i <= 10; i++ {
+		customerEnter(i, chairs)
 	}
-}
-
-func customerThread(id int) {
-	customerMutex.Lock()
-	if customers == maxCustomers {
-		customerMutex.Unlock()
-		balk()
-	}
-	customers++
-	customerMutex.Unlock()
-
-	fmt.Println("Customer", id, "entering shop")
-	customer <- struct{}{}
-	<-barber
-	getHairCut()
-	customerDone <- struct{}{}
-	<-barberDone
-	fmt.Println("Customer", id, "leaving shop")
-}
-
-func barberThread() {
-	for {
-		<-customer
-		cutHair()
-		barber <- struct{}{}
-		<-customerDone
-		barberDone <- struct{}{}
-	}
-}
-
-func getHairCut() {
-	fmt.Println("Getting hair cut")
-}
-
-func cutHair() {
-	fmt.Println("Cutting hair")
-}
-
-func balk() {
-	fmt.Println("Shop is full, customer unable to enter")
+	cancel()
+	wg.Wait()
+	close(wakeBell)
+	close(chairs)
+	fmt.Println("End")
 }
