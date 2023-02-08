1ed841f1691bcf7367bb31804e1530d607686723
kunakorn topurin <97731297+Bright-Kunakorn@users.noreply.github.com>
2023-02-08 20:03:09 +0700

    Update HW1_2_630510613.go

--- HW1_2_630510613.go
+++ HW1_2_630510613.go
@@ -3,7 +3,6 @@ import (
 	"fmt"
 	"time"
 )
-
 // PlayPing simulates the "bob" player in a game of ping-pong.
 // It receives messages on the "ping" channel and sends responses on the "pong" channel.
 func PlayPing(ping <-chan string, pong chan<- string) {
